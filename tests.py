a1ba77754b86007a2d1ca99cb40e01dc428c61f8
BurnoKerner <dino_kerner@hotmail.com>
2020-12-13 22:18:14 -0300

    added all white moves tests

--- tests.py
+++ tests.py
@@ -71,9 +71,36 @@ class test_sample(unittest.TestCase):
         self.assertEqual(result[0], valid_black_moves_sample[0][0:4])
 
     def test_valid_white_move_pawn(self):
-        # 
-        board_string_sample_1 = "                                                                                                                       P                                                        q q              P                    P                                         "
-        valid_white_moves_sample = [[7, 7, 6, 7, 4], [7, 7, 6, 7, 1], [12, 1, 11, 0, 10], [12, 1, 11, 2, 10], [12, 1, 10, 1, 3], [12, 1, 11, 1, 1], [13, 6, 11, 6, 3], [13, 6, 12, 6, 1]]
-        get_board_sample_1 = get_board(board_string_sample_1)
+        board_string_sample_pawns = "                                                                                                                       P                                                        q q              P                    P                                         "
+        valid_white_pawns_moves_sample = [[7, 7, 6, 7, 4], [7, 7, 6, 7, 1], [12, 1, 11, 0, 10], [12, 1, 11, 2, 10], [12, 1, 10, 1, 3], [12, 1, 11, 1, 1], [13, 6, 11, 6, 3], [13, 6, 12, 6, 1]]
+        get_board_sample_1 = get_board(board_string_sample_pawns)
         result = valid_white_move(get_board_sample_1)
-        self.assertEqual(result, valid_white_moves_sample)
+        self.assertEqual(result, valid_white_pawns_moves_sample)
+
+    def test_valid_white_move_queen(self):
+        board_string_sample_queens = "                                                                                                                                  Q                                             qqq             qQq             qqq                                             "
+        valid_white_queens_moves_sample = [[12, 1, 11, 1, 5], [12, 1, 12, 2, 5], [12, 1, 12, 0, 5], [12, 1, 13, 1, 5], [12, 1, 11, 0, 5], [12, 1, 13, 0, 5], [12, 1, 11, 2, 5], [12, 1, 13, 2, 5]]
+        get_board_sample_2 = get_board(board_string_sample_queens)
+        result = valid_white_move(get_board_sample_2)
+        self.assertEqual(result, valid_white_queens_moves_sample)
+
+    def test_valid_white_move_rook(self):
+        board_string_sample_rook = "                                                                                                                                  R                                              q              qRq              q                                              "
+        valid_white_rooks_moves_sample = [[8, 2, 7, 2, 1], [8, 2, 8, 3, 1], [8, 2, 8, 1, 1], [8, 2, 9, 2, 2], [12, 1, 11, 1, 10], [12, 1, 12, 2, 10], [12, 1, 12, 0, 10], [12, 1, 13, 1, 10]]
+        get_board_sample_3 = get_board(board_string_sample_rook)
+        result = valid_white_move(get_board_sample_3)
+        self.assertEqual(result, valid_white_rooks_moves_sample)
+
+    def test_valid_white_move_bishop(self):
+        board_string_sample_bishop = "                                                                                                                                                                                qqq             qBq             qqq                                             "
+        valid_white_bishops_moves_sample = [[12, 1, 11, 0, 10], [12, 1, 13, 0, 10], [12, 1, 11, 2, 10], [12, 1, 13, 2, 10]]
+        get_board_sample_4 = get_board(board_string_sample_bishop)
+        result = valid_white_move(get_board_sample_4)
+        self.assertEqual(result, valid_white_bishops_moves_sample)
+
+    def test_valid_white_move_king(self):
+        board_string_sample_kings = "                                                                                                                                  K                                             qqq             qKq             qqq                                             "
+        valid_white_kings_moves_sample = [[8, 2, 7, 2, 1], [8, 2, 8, 3, 1], [8, 2, 8, 1, 1], [8, 2, 9, 2, 2], [8, 2, 7, 1, 1], [8, 2, 9, 1, 1], [8, 2, 7, 3, 1], [8, 2, 9, 3, 1], [12, 1, 11, 1, 10], [12, 1, 12, 2, 10], [12, 1, 12, 0, 10], [12, 1, 13, 1, 10], [12, 1, 11, 0, 10], [12, 1, 13, 0, 10], [12, 1, 11, 2, 10], [12, 1, 13, 2, 10]]
+        get_board_sample_5 = get_board(board_string_sample_kings)
+        result = valid_white_move(get_board_sample_5)
+        self.assertEqual(result, valid_white_kings_moves_sample)
